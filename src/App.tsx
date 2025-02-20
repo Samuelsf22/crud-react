8d2ba6d089ad184aa6dabf4411fe151946aae6d5
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com>
2025-02-20 18:02:34 -0500

    feat: implement ProtectedRoute component for enhanced route protection and authentication flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Login from "./components/auth/Login";
 import Layout from "./layout/Layout";
 import CreateUser from "./components/auth/CreateUser";
+import ProtectedRoute from "./components/utils/ProtectedRoute";
 
 export default function App() {
-  const isAuthenticated = localStorage.getItem("auth_token") !== null;
+  const auth = !!localStorage.getItem("auth_token");
 
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route
-            path="/"
-            element={isAuthenticated ? <Layout /> : <Navigate to="/login" />}
-          />
+            element={
+              <ProtectedRoute canActivate={auth} redirectPath="/login" />
+            }
+          >
+            <Route path="/" element={<Layout />} />
+          </Route>
           <Route
-            path="/login"
-            element={isAuthenticated ? <Navigate to="/" /> : <Login />}
-          />
-          <Route
-            path="/signup"
-            element={isAuthenticated ? <Navigate to="/" /> : <CreateUser />}
-          />
+            element={<ProtectedRoute canActivate={!auth} redirectPath="/" />}
+          >
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<CreateUser />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
